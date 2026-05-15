baa3b2a96343bdfdc0f7b4daed36e6216a5e7510
Portfolio-Sakatani <tonytomotokyo@gmail.com>
2026-05-15 12:51:03 +0900

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,6 +1,8 @@
+# ECSタスク実行ロール定義
 resource "aws_iam_role" "ecs_task_execution" {
   name = "${var.project}-ecs-exec-role"
 
+# ECSタスクへの使用許可
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -13,6 +15,7 @@ resource "aws_iam_role" "ecs_task_execution" {
   })
 }
 
+# 作成したIAMロールに、AWS管理の「AmazonECSTaskExecutionRolePolicy」をアタッチ(ECRからのイメージ取得やCloudWatchへのログ出力が可能に)
 resource "aws_iam_role_policy_attachment" "ecs_exec_attach" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
