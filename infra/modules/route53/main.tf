d6ec322dcb7375392207753f16c9f69e819da87e
Portfolio-Sakatani <tonytomotokyo@gmail.com>
2026-05-15 12:52:25 +0900

    Update main.tf

--- infra/modules/route53/main.tf
+++ infra/modules/route53/main.tf
@@ -1,8 +1,10 @@
+# Route 53のホストゾーンにDNSレコード作成定義
 resource "aws_route53_record" "this" {
   zone_id = var.zone_id
   name    = var.domain_name
   type    = "A"
 
+  # ALB紐づけエイリアスレコード設定
   alias {
     name                   = var.alb_dns_name
     zone_id                = var.alb_zone_id
